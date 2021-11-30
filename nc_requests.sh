cbb0b85b13f9fa866ac7ae1056c5ab4cba82c184
antoine <antoine@antoine>
2021-11-30 22:29:35 +0100

    Oops, this is the latest version

--- nc_requests.sh
+++ nc_requests.sh
@@ -4,14 +4,14 @@ while [ True ]
 do
 				IP="10.0."$(((RANDOM % 254)+1))"."$(((RANDOM%254)+1)) #Voir si on peut faire en sorte de mettre des IP hors réseaux ?
 				echo $IP
-				#ifconfig eth0 $IP netmask 255.255.0.0 #Voir si on peut modifier le masque de sous réseau ?
-				sleep 5
+				ifconfig eth0 $IP netmask 255.255.0.0 #Voir si on peut modifier le masque de sous réseau ?
+				sleep 3
 				id=22
 				while [ $id -lt 58 ]
 				do
-								echo "nc 10.0.$id.199 10003"
-								echo "nc 10.0.$id.199 10002"
-								echo "nc 10.0.$id.199 10000"
+								nc 10.0.$id.199 10003
+								nc 10.0.$id.199 10002
+								nc 10.0.$id.199 10000
 								id=$((id+1))
 				done
 				echo "==="
@@ -19,7 +19,7 @@ done
 
 ##echo "auto lo
 #iface eth0 inet static
-#		address $IP" > /etc/network/interfaces
-
+#		address $IP
+#		netmask 255.255.0.0" > /etc/network/interfaces
 #service networking stop
 #service networking start
