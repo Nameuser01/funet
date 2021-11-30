c75af86297f436af01ad73789931e0439ccf34d9
antoine <antoine@antoine>
2021-11-30 22:31:22 +0100

    Looks nicer that way

--- nc_requests.sh
+++ nc_requests.sh
@@ -2,19 +2,19 @@
 #FAIRE UN CLONE DE LA VM
 while [ True ]
 do
-				IP="10.0."$(((RANDOM % 254)+1))"."$(((RANDOM%254)+1)) #Voir si on peut faire en sorte de mettre des IP hors réseaux ?
-				echo $IP
-				ifconfig eth0 $IP netmask 255.255.0.0 #Voir si on peut modifier le masque de sous réseau ?
-				sleep 3
-				id=22
-				while [ $id -lt 58 ]
-				do
-								nc 10.0.$id.199 10003
-								nc 10.0.$id.199 10002
-								nc 10.0.$id.199 10000
-								id=$((id+1))
-				done
-				echo "==="
+	IP="10.0."$(((RANDOM % 254)+1))"."$(((RANDOM%254)+1)) #Voir si on peut faire en sorte de mettre des IP hors réseaux ?
+	echo $IP
+	ifconfig eth0 $IP netmask 255.255.0.0 #Voir si on peut modifier le masque de sous réseau ?
+	sleep 3
+	id=22
+	while [ $id -lt 58 ]
+	do
+		nc 10.0.$id.199 10003
+		nc 10.0.$id.199 10002
+		nc 10.0.$id.199 10000
+		id=$((id+1))
+	done
+	echo "==="
 done
 
 ##echo "auto lo
